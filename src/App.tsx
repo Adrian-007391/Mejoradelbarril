98ed5ef9b41245f0edd0de0193c27c93156040e5
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro-2.local>
2022-08-29 10:20:33 -0500

    Startpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
 import styled from "styled-components";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Startpage from "./Pages/Startpage/Startpage";
 function App() {
   const Title = styled.h1`
     font-family: Arial, Helvetica, sans-serif;
     text-align: center;
   `;
   return (
-    <div>
-      <Title>Cervezas</Title>
-    </div>
+    <Router>
+      <Routes>
+        <Route element={<Startpage />} path="/" />
+      </Routes>
+    </Router>
   );
 }
 
